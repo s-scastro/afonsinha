206ac0c4e7c3a4cfa64895080ee465c18e382a4e
le7cia-sys <le7cia@gmail.com>
2025-10-29 22:36:12 -0400

    Add files via upload

---
+++ css/js/script.js
@@ -0,0 +1,5 @@
+// Quando clicar no botão, mostra uma mensagem
+document.getElementById("botao").addEventListener("click", function() {
+    alert("Nova notícia: Beyoncé e Taylor Swift vão lançar músicas novas! 🎶");
+  });
+  
